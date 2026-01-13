734bfee92773844f4a421cb244d91f05c383ad1d
Luk, Marek <marek.luk@sap.com>
2026-01-13 23:06:41 +0100

    fix

--- scripts/publish_assets.sh
+++ scripts/publish_assets.sh
@@ -12,7 +12,7 @@ release_tag=$2
 release_id=$3
 
 repository="${REPOSITORY:-kyma-project/api-gateway}"
-github_api_repo_url="https://api.github.com/repos/${repository}"
+github_upload_repo_url="https://uploads.github.com/repos/${repository}"
 
 echo "Generate manifests"
 IMG="${image_name}:${release_tag}" VERSION=release_tag make generate-manifests
@@ -20,23 +20,23 @@ IMG="${image_name}:${release_tag}" VERSION=release_tag make generate-manifests
 echo "Publish manager yaml"
 manager_yaml_asset_name="api-gateway-manager.yaml"
 manager_yaml_asset_path="api-gateway-manager.yaml"
-curl -s -S -f -L \
+curl -v -S -f -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   -H "Content-Type: application/octet-stream" \
   --data-binary @"${manager_yaml_asset_path}" \
-  "${github_api_repo_url}/releases/${release_id}/assets?name=${manager_yaml_asset_name}"
+  "${github_upload_repo_url}/releases/${release_id}/assets?name=${manager_yaml_asset_name}"
 
 echo "Publish default CR"
 default_cr_asset_name="apigateway-default-cr.yaml"
 default_cr_path="config/samples/operator_v1alpha1_apigateway.yaml"
-curl -s -S -f -L \
+curl -v -S -f -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   -H "Content-Type: application/octet-stream" \
   --data-binary @"${default_cr_path}" \
-  "${github_api_repo_url}/releases/${release_id}/assets?name=${default_cr_asset_name}"
+  "${github_upload_repo_url}/releases/${release_id}/assets?name=${default_cr_asset_name}"
